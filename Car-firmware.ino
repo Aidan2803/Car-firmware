cbfcade2531f43096f7a1f660158d5f0e6915ae5
aidan <aid2803@ukr.net>
2023-08-09 01:56:09 +0300

    Update car-firmware with lights reacting to button

--- Car-firmware.ino
+++ Car-firmware.ino
@@ -1,73 +1,113 @@
 /* Car firmware */
 
-const int joystick_pin_x = 0;     // A0
-const int joystick_pin_y = 1;     // A1
-const int joystick_pin_butt = 2;  // D2
+/* Pins */
+
+const int joystickPinX = 0;     // A0
+const int joystickPinY = 1;     // A1
+const int joystickPinButt = 2;  // D2
 
 const int motorFront_IN_1 = 3;  // D3
 const int motorFront_IN_2 = 4;  // D4
 const int motorBack_IN_1 = 5;   // D5
 const int motorBack_IN_2 = 6;   // D6
 
+const int buttonPin = 7;         // D7
+const int ledLightLeftPin = 8;   // D8
+const int ledLightRightPin = 9;  // D9
+
+/* Button variables */
+int buttonState = 0;
+int lastButtonState = 0;
+int lastDebounceTime = 0;
+int debounceDelay = 0;
+
+/* Led light states */
+int ledLeftLightState = 0;
+int ledRightLightState = 0;
+
 /* Joysticks */
 
-int JoyStickGetX() { return analogRead(joystick_pin_x); }
+int JoyStickGetX() { return analogRead(joystickPinX); }
 
-int JoyStickGetY() { return analogRead(joystick_pin_y); }
+int JoyStickGetY() { return analogRead(joystickPinY); }
 
-bool JoyStcikIsButtonPressed() { return digitalRead(joystick_pin_butt); }
+bool JoyStcikIsButtonPressed() { return digitalRead(joystickPinButt); }
 
 /* Motors */
 
 void SpinMotor(int x_value) {
-
-  if(x_value >= 512){  
+  if (x_value >= 512) {
     // Spin forward Front motor
-  digitalWrite(motorFront_IN_1, HIGH);
-  digitalWrite(motorFront_IN_2, LOW);
+    digitalWrite(motorFront_IN_1, HIGH);
+    digitalWrite(motorFront_IN_2, LOW);
 
-  // Spin forward Back motor
-  digitalWrite(motorBack_IN_1, HIGH);
-  digitalWrite(motorBack_IN_2, LOW);
-  delay(100);
-    }
-
-  else if (x_value >= 201 && x_value <= 450){  // Spin back Front motor
-  digitalWrite(motorFront_IN_1, LOW);
-  digitalWrite(motorFront_IN_2, HIGH);
-
-  // Spin back Back motor
-  digitalWrite(motorBack_IN_1, LOW);
-  digitalWrite(motorBack_IN_2, HIGH);
-  delay(100);
-}
-
-else if(x_value >= 451 && x_value <= 511){  // Spin neutral Front motor
-  digitalWrite(motorFront_IN_1, LOW);
-  digitalWrite(motorFront_IN_2, LOW);
-
-  // Spin neutral Back motor
-  digitalWrite(motorBack_IN_1, LOW);
-  digitalWrite(motorBack_IN_2, LOW);
-  delay(100);}
-
-else if(x_value >= 0 && x_value <= 200){ 
-  // Stop Front motor
-  digitalWrite(motorFront_IN_1, HIGH);
-  digitalWrite(motorFront_IN_2, HIGH);
-
-  // Stop back Back motor
-  digitalWrite(motorBack_IN_1, HIGH);
-  digitalWrite(motorBack_IN_2, HIGH);
-  delay(100);
+    // Spin forward Back motor
+    digitalWrite(motorBack_IN_1, HIGH);
+    digitalWrite(motorBack_IN_2, LOW);
+    delay(100);
   }
 
+  else if (x_value >= 201 && x_value <= 450) {  // Spin back Front motor
+    digitalWrite(motorFront_IN_1, LOW);
+    digitalWrite(motorFront_IN_2, HIGH);
+
+    // Spin back Back motor
+    digitalWrite(motorBack_IN_1, LOW);
+    digitalWrite(motorBack_IN_2, HIGH);
+    delay(100);
+  }
+
+  else if (x_value >= 451 && x_value <= 511) {  // Spin neutral Front motor
+    digitalWrite(motorFront_IN_1, LOW);
+    digitalWrite(motorFront_IN_2, LOW);
+
+    // Spin neutral Back motor
+    digitalWrite(motorBack_IN_1, LOW);
+    digitalWrite(motorBack_IN_2, LOW);
+    delay(100);
+  }
+
+  else if (x_value >= 0 && x_value <= 200) {
+    // Stop Front motor
+    digitalWrite(motorFront_IN_1, HIGH);
+    digitalWrite(motorFront_IN_2, HIGH);
+
+    // Stop back Back motor
+    digitalWrite(motorBack_IN_1, HIGH);
+    digitalWrite(motorBack_IN_2, HIGH);
+    delay(100);
+  }
+}
+
+void ButtonIsPressed() {
+  int currentButtonState = digitalRead(buttonPin);
+  Serial.println(currentButtonState);
+
+  if (currentButtonState != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (currentButtonState != buttonState) {
+      buttonState = currentButtonState;
+
+      if (buttonState == HIGH) {
+        ledLeftLightState = !ledLeftLightState;
+        ledRightLightState = !ledRightLightState;
+      }
+    }
+  }
+
+  digitalWrite(ledLightLeftPin, ledLeftLightState);
+  digitalWrite(ledLightRightPin, ledRightLightState);
+
+  lastButtonState = currentButtonState;
 }
 
 void setup() {
-  pinMode(joystick_pin_x, INPUT);
-  pinMode(joystick_pin_y, INPUT);
-  pinMode(joystick_pin_butt, INPUT_PULLUP);
+  pinMode(joystickPinX, INPUT);
+  pinMode(joystickPinY, INPUT);
+  pinMode(joystickPinButt, INPUT_PULLUP);
 
   pinMode(motorFront_IN_1, OUTPUT);
   pinMode(motorFront_IN_2, OUTPUT);
@@ -76,7 +116,8 @@ void setup() {
 }
 
 void loop() {
-  Serial.print(JoyStickGetX());
-  Serial.print("\n");
+//  Serial.print(JoyStickGetX());
+//  Serial.print("\n");
+  ButtonIsPressed();
   SpinMotor(10);
 }
